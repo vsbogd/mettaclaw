244423d03df62290389bd6af5b20260c1efa7f55
Patrick Hammer <patham9@gmail.com>
2026-04-20 13:47:52 +0200

    Update: do not read before process wait, do not rely on string concat to avoid quoting hell, do handle OOM kill too, handle resource cleanup in all cases, use tmp file instead of pipe to avoid blocking

--- src/skills.pl
+++ src/skills.pl
@@ -1,11 +1,40 @@
 %Gets shell command return, plus the process if time limit is not met, returning timeout_error:
-shell(Cmd, Out) :- format(string(SafeCmd), "timeout -k 1s 10s sh -c '~w'", [Cmd]),
-                   process_create(path(sh), ['-c', SafeCmd], [ stdout(pipe(S)), stderr(pipe(S)), process(P)]),
-                   setup_call_cleanup(true,
-                                      read_string(S, _, Text),
-                                      close(S)),
-                   process_wait(P, Status),
-                   ( Status = exit(124) -> Out = timeout_error
-                                         ; Out = Text ).
+shell(Cmd, Out) :-
+    tmp_file_stream(text, TmpFile, TmpInit),
+    close(TmpInit),
+    open(TmpFile, write, TmpOut, [type(text)]),
+    catch(
+        setup_call_cleanup(
+            process_create(
+                path(timeout),
+                ['-k', '1s', '5s', 'sh', '-c', Cmd],
+                [ stdout(stream(TmpOut)),
+                  stderr(stream(TmpOut)),
+                  process(P)
+                ]
+            ),
+            (
+                process_wait(P, Status),
+                close(TmpOut),
+                read_file_to_string(TmpFile, Text, [])
+            ),
+            (
+                catch(close(TmpOut), _, true),
+                catch(delete_file(TmpFile), _, true)
+            )
+        ),
+        E,
+        (
+            catch(close(TmpOut), _, true),
+            catch(delete_file(TmpFile), _, true),
+            throw(E)
+        )
+    ),
+    ( Status = exit(124) -> Out = timeout_error
+    ; Status = exit(137) -> Out = timeout_error
+    ; Status = killed(_) -> Out = timeout_error
+    ; Out = Text
+    ).
+
 
 first_char(Str, C) :- sub_string(Str, 0, 1, _, C).
