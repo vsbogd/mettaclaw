4f3d7e5756da36bb358cce625b54e5b76a027b52
Vitaly Bogdanov <vsbogd@gmail.com>
2026-06-30 21:58:38 +0300

    Add plugin API documentation

--- src/plugin.py
+++ src/plugin.py
@@ -1,3 +1,9 @@
+"""
+This is OmegaClaw internal API which is built on the top of the plugins API.
+Most of the functions here calls corresponding functions of the registered
+plugins.
+"""
+
 import pathlib
 import yaml
 import importlib
@@ -9,12 +15,18 @@ _plugins = {}
 _commchannel: pluginapi.CommChannel = None
 _llmprovider: pluginapi.LLMProvider = None
 
-def error(func, text):
+def _error(func, text):
     error = f"{func}: {text}"
     print(error)
     raise RuntimeError(error)
 
 def initPlugins():
+    """Loads the list of the plugins from ./config/plugins.yaml file and then
+    loads each plugin from the list using the specified loader. YAML file
+    contains the list of plugins each specified by three fields:
+    - name - required, must be unique
+    - loader - required, must be either "python" or "metta"
+    - location - optional, path to the plugin module used by Python loader"""
     global _plugins, _REPO
 
     plugins_path = _REPO.joinpath("./config/plugins.yaml")
@@ -25,36 +37,43 @@ def initPlugins():
 
         name = p.get("name")
         if name is None:
-            error("initPlugins", f"name field is empty, file: {plugins_path}, index: {i}")
+            _error("initPlugins", f"name field is empty, file: {plugins_path}, index: {i}")
         if name in _plugins:
-            error("initPlugins", f"name '{name}' is not unique")
+            _error("initPlugins", f"name '{name}' is not unique")
 
         loader = p.get("loader", "metta")
         if loader == "python":
-            initPythonPlugin(p)
+            _initPythonPlugin(p)
         elif loader == "metta":
-            initMettaPlugin(p)
+            _initMettaPlugin(p)
         else:
-            error("initPlugins", f"loader '{loader}' is not implemented")
+            _error("initPlugins", f"loader '{loader}' is not implemented")
 
 class PythonPlugin:
+    """Class to wrap the reference to the Python module and keep it inside
+    plugins registry"""
 
     def __init__(self, mod):
         self.mod = mod
 
-def initPythonPlugin(plugin):
+def _initPythonPlugin(plugin):
+    """Python plugin loader implementation. If location of the plugin is
+    specified it imports "<location>/<name>.py" file. Imports <name> Python
+    module otherwise. Calls "loadOmegaClawPlugin" function from the imported
+    module. This is the point where plugin's code gets control and should
+    register appropriate callbacks."""
     global _plugins, _REPO
 
     name = plugin.get("name")
     location = plugin.get("location")
     mod = None
     if location is None:
-        print(f"initPythonPlugin: loading {name} plugin from PYTHONPATH using Python module loader")
+        print(f"_initPythonPlugin: loading {name} plugin from PYTHONPATH using Python module loader")
         mod = importlib.import_module(name)
     else:
         location = pathlib.Path(location.format(REPO=_REPO))
         location = location.joinpath(f"{name}.py").resolve()
-        print(f"initPythonPlugin: loading {name} plugin from {location} using Python module loader")
+        print(f"_initPythonPlugin: loading {name} plugin from {location} using Python module loader")
         spec = importlib.util.spec_from_file_location(name, location)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
@@ -62,17 +81,18 @@ def initPythonPlugin(plugin):
     if mod is not None:
         _plugins[name] = PythonPlugin(mod)
     else:
-        error("initPythonPlugin", f"Couldn't find Python module {name}")
+        _error("_initPythonPlugin", f"Couldn't find Python module {name}")
 
     if not hasattr(mod, "loadOmegaClawPlugin"):
-        error("initPythonPlugin", f"No loadOmegaClawPlugin() function is implemented by plugin {name}")
+        _error("_initPythonPlugin", f"No loadOmegaClawPlugin() function is implemented by plugin {name}")
     plugin_loader = getattr(mod, "loadOmegaClawPlugin")
     plugin_loader()
 
-def initMettaPlugin(plugin):
+def _initMettaPlugin(plugin):
     raise NotImplementedError()
 
 def listToDict(list):
+    """Converts list of <key>=<value> pairs into Python dictionary"""
     print(f"listToDict: list {list}")
     dict = {}
     list = [s.split("=", 1) for s in list]
@@ -81,27 +101,33 @@ def listToDict(list):
     return dict
 
 def commChannelConfig(commchannel, config):
+    """Select and configure one of the communication channels registered by
+    plugins"""
     global _commchannel
     _commchannel = pluginapi._commChannelRegistry.get(commchannel, None)
     if _commchannel is None:
-        error("commChannelConfig", f"Communication channel plugin {commchannel} is not registered")
+        _error("commChannelConfig", f"Communication channel plugin {commchannel} is not registered")
     _commchannel.config(config)
 
 def commChannelReceive():
+    """Receive message from selected communication channel"""
     global _commchannel
     return _commchannel.receive()
 
 def commChannelSend(message):
+    """Send message via selected communication channel"""
     global _commchannel
     _commchannel.send(message)
 
 def llmProviderConfig(provider, config):
+    """Select and configure one of the LLM providers registered by plugins"""
     global _llmprovider
     _llmprovider = pluginapi._llmProviderRegistry.get(provider, None)
     if _llmprovider is None:
-        error("llmProviderConfig", f"LLM provider plugin {provider} is not registered")
+        _error("llmProviderConfig", f"LLM provider plugin {provider} is not registered")
     _llmprovider.config(config)
 
 def llmProviderChat(prompt, max_tokens, reasoning_mode):
+    """Chat via selected LLM provider"""
     global _llmprovider
     return _llmprovider.chat(prompt, max_tokens, reasoning_mode)
--- src/pluginapi.py
+++ src/pluginapi.py
@@ -1,35 +1,49 @@
 # FIXME: rename this module to be unique for instance omegaclaw_plugin_api
-from collections.abc import Callable
+"""
+OmegaClaw API which should be used to implement extensions.
+"""
 
 _commChannelRegistry = {}
 _llmProviderRegistry = {}
 
 class CommChannel:
+    """Communication channel implementation"""
 
     def config(self, config: dict) -> None:
+        """Configure communication channel. Receives the subset of the command
+        line parameters which are <key>=<value> pairs"""
         raise NotImplementedError()
 
     def receive(self) -> str:
+        """Receive message from the communication channel"""
         raise NotImplementedError()
 
     def send(self, message: str) -> None:
+        """Send message via the communication channel"""
         raise NotImplementedError()
 
 def registerCommChannel(id: str, channel: CommChannel) -> None:
+    """Register communication channel in the registry"""
     global _commChannelRegistry
     print(f"registerCommChannel: registering communication channel {id}")
     _commChannelRegistry[id] = channel
 
 
 class LLMProvider:
+    """LLM provider implementation"""
 
     def config(self, config: dict) -> None:
+        """Configure LLM provider. Receives the subset of the command line
+        parameters or configuration file keys which are started by "<id>_"
+        prefix"""
         raise NotImplementedError()
 
     def chat(self, prompt: str, max_tokens: int = 6000, reasoning_mode: str = "medium") -> str:
+        """Chat with LLM provider"""
         raise NotImplementedError()
 
 def registerLLMProvider(id: str, provider: LLMProvider) -> None:
+    """Register LLM provider in the registry"""
     global _llmProviderRegistry
     print(f"registerLLMProvider: registering LLM provider {id}")
     _llmProviderRegistry[id] = provider
